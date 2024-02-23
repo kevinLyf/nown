1e4b19c73d3ea81d4d06b49a9964ecb3596611be
Kevin Feitosa <kevin.feitosa11@outlook.com>
2024-02-23 12:07:23 -0300

    feat: add header

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,8 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        @vite('resources/css/app.css')
-        <title>Laravel</title>
+@extends('layouts.main')
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-    </head>
-    <body>
+@section('title', 'nown')
 
-    </body>
-</html>
+@section('content')
+
+<h1>alo</h1>
+@endsection
