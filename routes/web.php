8635a4159c3819aec0fb331f4f38b81e055490e3
Kevin Feitosa <kevin.feitosa11@outlook.com>
2024-02-23 16:25:30 -0300

    refactor: add prefix posts at post controller

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ use \App\Http\Controllers\PostController;
 */
 
 Route::get('/', [PostController::class, 'index']);
-Route::get('/posts/create', [PostController::class, 'create']);
-Route::post('/posts', [PostController::class, 'store']);
-Route::get('/posts/{id}', [PostController::class, 'show']);
+
+Route::prefix('/posts')->group(function () {
+    Route::post('/', [PostController::class, 'store']);
+    Route::get('/create', [PostController::class, 'create']);
+    Route::get('/{id}', [PostController::class, 'show']);
+});
+
