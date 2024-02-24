88b746905dbfbc13b1702aa89be322657b324767
Kevin Feitosa <kevin.feitosa11@outlook.com>
2024-02-23 21:31:26 -0300

    refactor: change dark color

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -96,7 +96,7 @@
                                           d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
                                 </svg>
 
-                                <h4 class="text-xl font-bold">{{ $comment->user->name }}</h4>
+                                <h4 class="text-xl font-bold text-gray-600">{{ $comment->user->name }}</h4>
                             </div>
                             <span
                                 class="text-sm text-gray-500">{{ $comment->created_at->format('h:i - D, m Y') }}</span>
